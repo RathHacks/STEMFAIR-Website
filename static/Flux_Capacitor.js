9e01fee448e81f25fbbf9810494b0736f1f8ccae
Brentan Rath <brentanrath@gmail.com>
2026-01-15 12:43:07 -0500

    added SUPER SECRET HACKER PROTECTION OMG

---
+++ static/Flux_Capacitor.js
@@ -0,0 +1,9 @@
+async function Hacker_Protection_Flux_Capacitor() {
+    try {
+        console.log('^w^ a floof as where')
+    } catch (error) {
+        console.log('Flux Capacitor Error: Unable to load Flux Capacitor at this time.');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', Hacker_Protection_Flux_Capacitor);
